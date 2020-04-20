6594815dc4a252eaddf3cb7c2c316208fead2a84
amir mazinani <info.mazinani@gmail.com>
2020-04-20 09:43:20 +0430

    add evolution function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 
 def clear(): return os.system('clear')
@@ -13,3 +14,24 @@ def draw(u, w, h):
             else:
                 print(" ", end="")
         print("\n")
+
+
+def evolution(u, w, h):
+    new = [[0 for x in range(w)] for y in range(h)]
+
+    for x in range(w):
+        for y in range(h):
+            lives = 0
+            for xd in range(x-1, x+2):
+                for yd in range(y-1, y+2):
+                    if u[(yd + h) % h][(xd + w) % w]:
+                        lives += 1
+            if u[y][x]:
+                lives -= 1
+            if lives == 3 or (lives == 2 and u[y][x]):
+                new[y][x] = 1
+            else:
+                new[y][x] = 0
+    for y in range(h):
+        for x in range(w):
+            u[y][x] = new[y][x]
